4e158b94f7b226dd7c23d94a8feca83b4626f0cf
Jamila Mahamed <j.mahamed95@icloud.com>
2023-03-26 23:51:04 +0200

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            <img src="../images/icon-cooking.jpg" className="nav--logo" alt="logo" />
+            <h3 className="nav-name">Simple Recipes</h3>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/components/Recipes.js
@@ -0,0 +1,34 @@
+
+
+const Recipes = () => {
+    return (
+        <div className="container">
+            <div className="recipe-card">
+                <img src="../images/waffles.jpg" alt="waffles" />
+                <div className="card-text">
+                    <h3>Wake up to smell of waffles</h3>
+                    <p>The gorgeous scent of vanilla infused waffles. Now that's the perfect start to the weekend</p>
+                    <div className="text-bottom">Preparation time: 10Min Cook time: 15Min</div>
+                </div>
+            </div>
+            <div className="recipe-card">
+                <img src="../images/waffles.jpg" alt="waffles" />
+                <div className="card-text">
+                    <h3>Wake up to smell of waffles</h3>
+                    <p>The gorgeous scent of vanilla infused waffles. Now that's the perfect start to the weekend</p>
+                    <div className="text-bottom">Preparation time: 10Min Cook time: 15Min</div>
+                </div>
+            </div>
+            <div className="recipe-card">
+                <img src="../images/waffles.jpg" alt="waffles" />
+                <div className="card-text">
+                    <h3>Wake up to smell of waffles</h3>
+                    <p>The gorgeous scent of vanilla infused waffles. Now that's the perfect start to the weekend</p>
+                    <div className="text-bottom">Preparation time: 10Min Cook time: 15Min</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Recipes
