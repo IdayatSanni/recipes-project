fa9289bef3021b400a36abf7ffea3c98755627f4
IdayatSanni <idayatsanni@gmail.com>
2023-04-01 01:25:53 +0100

    Add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Recipes from "./components/Recipes";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Recipes />
+      <Button />
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,5 @@
+const Button = () => {
+  return <button className="btn btn-danger">Click To Get More Recipes</button>;
+};
+
+export default Button;
